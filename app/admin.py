74d5f9554e12ae917579268dc4e667657786fe1f
Jhordan Gabriel <gabrieljhordang@gmail.com>
2021-05-31 16:30:00 -0300

    Criando as rotas e os templates para admin/plano

--- app/admin.py
+++ app/admin.py
@@ -18,6 +18,12 @@ class Funcionario():
         self.salario = salario
         self.cargo = cargo
 
+class Plano:
+    def __init__(self, nome, preco, descricao):
+        self.nome = nome
+        self.preco = preco
+        self.descricao = descricao
+
 # Admins
 admin1 = Admin('luan', 'Luan Marques', '1234')
 admin2 = Admin('Nico', 'Nico Steppat', '7a1')
@@ -32,6 +38,11 @@ funcionario2 = Funcionario('Pedro','4234447890',3500.00,'Dev')
 funcionario3 = Funcionario('Maria','4235677890',3000.00,'UX')
 trabalhadores = [funcionario1, funcionario2, funcionario3]
 
+# Planos
+plano1 = Plano('Básico',89.00, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ornare, turpis vitae faucibus tincidunt, erat sem commodo sem, eget dapibus leo nisi non est.')
+plano2 = Plano('Intermediario',119.00, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ornare, turpis vitae faucibus tincidunt, erat sem commodo sem, eget dapibus leo nisi non est.')
+plano3 = Plano('Premium',159.00, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ornare, turpis vitae faucibus tincidunt, erat sem commodo sem, eget dapibus leo nisi non est.')
+listaplanos = [plano1,plano2,plano3]
 
 # Home
 @app.route('/admin')
@@ -82,6 +93,26 @@ def criarfunc():
     trabalhadores.append(func)
     return redirect(url_for('funcionarios'))
 
+# Rotas dos Planos
+@app.route('/admin_planos')
+def planos():
+    if 'usuario_logado' not in session or session['usuario_logado'] == None:
+        return redirect(url_for('login', proxima=url_for('admin_planos')))
+    return render_template('admin_planos.html', titulo='Funcionarios', listaplanos = listaplanos)
 
+@app.route('/admin_novo_plano')
+def admin_planos():
+    if 'usuario_logado' not in session or session['usuario_logado'] == None:
+        return redirect(url_for('login', proxima=url_for('admin_novo_plano')))
+    return render_template('admin_criar_plano.html', titulo='Novo Plano')
+
+@app.route('/criarplano', methods=['POST',])
+def criarplano():
+    nome = request. form['nome']
+    preco = request. form['preco']
+    descricao = request. form['descricao']
+    plano = Plano( nome,preco,descricao)
+    listaplanos.append(plano)
+    return redirect(url_for('planos'))
 
 app.run(debug=True)
