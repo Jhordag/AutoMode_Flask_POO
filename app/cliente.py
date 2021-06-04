7e94e974a59bfb59051f47257b44cf62f76073ce
Perkhad <gabrieldamatamarques@gmail.com>
2021-06-04 16:25:39 -0300

    atualizado com página inicial

--- app/cliente.py
+++ app/cliente.py
@@ -1,13 +1,13 @@
 from flask import Flask, render_template, request, redirect, session, flash, url_for
+from app import app
 
-app = Flask(__name__)
-app.secret_key = 'BiaUFGPOO'
 
 class Plano:
     def __init__(self, nome, preco, descricao):
         self.nome = nome
         self.preco = preco
         self.descricao = descricao
+
 
 class Cliente:
     def __init__(self, empresa, cnpj, phone, email, senha):
@@ -17,8 +17,9 @@ class Cliente:
         self.email = email
         self.senha = senha
 
+
 class Cartao:
-    def __init__(self,parcela,numcart,dtv,cvv,nome,cpf):
+    def __init__(self, parcela, numcart, dtv, cvv, nome, cpf):
         self.parcela = parcela
         self.numcart = numcart
         self.dtv = dtv
@@ -26,25 +27,36 @@ class Cartao:
         self.nome = nome
         self.cpf = cpf
 
+
 # Clientes
-cliente1 = Cliente('JOJH','12345678901234','99999000011','jojh@jojh.com','1234')
-cliente2 = Cliente('POJ','12345278921234','99999000022','poj@poj.com','1357')
-cliente3 = Cliente('KOL','12345178411234','99999000033','poj@poj.com','2468')
+cliente1 = Cliente('JOJH', '12345678901234', '99999000011', 'jojh@jojh.com', '1234')
+cliente2 = Cliente('POJ', '12345278921234', '99999000022', 'poj@poj.com', '1357')
+cliente3 = Cliente('KOL', '12345178411234', '99999000033', 'poj@poj.com', '2468')
 clientes = {cliente1.email: cliente1,
             cliente2.email: cliente2,
             cliente3.email: cliente3}
 
-#Planos
-plano1 = Plano('Básico',89.00, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ornare, turpis vitae faucibus tincidunt, erat sem commodo sem, eget dapibus leo nisi non est.')
-plano2 = Plano('Intermediario',119.00, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ornare, turpis vitae faucibus tincidunt, erat sem commodo sem, eget dapibus leo nisi non est.')
-plano3 = Plano('Premium',159.00, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ornare, turpis vitae faucibus tincidunt, erat sem commodo sem, eget dapibus leo nisi non est.')
-listaplanos = [plano1,plano2,plano3]
+# Planos
+plano1 = Plano('Básico', 89.00,
+               'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ornare, turpis vitae faucibus tincidunt, erat sem commodo sem, eget dapibus leo nisi non est.')
+plano2 = Plano('Intermediario', 119.00,
+               'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ornare, turpis vitae faucibus tincidunt, erat sem commodo sem, eget dapibus leo nisi non est.')
+plano3 = Plano('Premium', 159.00,
+               'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ornare, turpis vitae faucibus tincidunt, erat sem commodo sem, eget dapibus leo nisi non est.')
+listaplanos = [plano1, plano2, plano3]
+
+
+@app.route("/")
+@app.route("/index")
+def home():
+    return render_template('index.html')
 
 
 # Home Cliente
-@app.route('/')
+@app.route('/cliente')
 def home():
     return render_template('cliente_home.html', titulo='Planos', listaplanos=listaplanos)
+
 
 @app.route('/home_login')
 def home_login():
@@ -52,11 +64,13 @@ def home_login():
         return redirect(url_for('login', proxima=url_for('home_login')))
     return render_template('cliente_home_login.html', titulo='Home', listaplanos=listaplanos)
 
-#Autenticação
+
+# Autenticação
 @app.route('/login')
 def login():
     proxima = request.args.get('proxima')
-    return render_template('cliente_login.html',proxima=proxima)
+    return render_template('cliente_login.html', proxima=proxima)
+
 
 @app.route('/cliente_autenticar', methods=['POST', ])
 def autenticar_cliente():
@@ -70,28 +84,32 @@ def autenticar_cliente():
     else:
         flash('Não logado, tente novamente!')
         return redirect(url_for('login'))
-    
+
+
 @app.route('/logout')
 def logout():
     session['usuario_logado'] = None
     flash('Nenhum usuário logado!')
     return redirect(url_for('home'))
 
-#Comprar Plano
+
+# Comprar Plano
 @app.route('/comprarcredit')
 def comprar():
     if 'usuario_logado' not in session or session['usuario_logado'] == None:
         return redirect(url_for('login', proxima=url_for('home_login')))
     return render_template('cliente_comprar.html', titulo='Comprar')
 
+
 @app.route('/autenticar_cartao', methods=['POST', ])
 def autenticar_cartao():
-    if request.form['numcart'] != None :
+    if request.form['numcart'] != None:
         flash('Compara feita  com sucesso!')
         return redirect(url_for('home_login'))
     else:
         flash('Não foi possivel fazer a comprar!')
         return redirect(url_for('login'))
+
 
 # Perfil Cliente
 @app.route('/perfil')
@@ -100,11 +118,13 @@ def perfil():
         return redirect(url_for('login', proxima=url_for('perfil')))
     return render_template('cliente_perfil.html', titulo='Perfil')
 
+
 @app.route('/usuarios')
 def usuarios():
     if 'usuario_logado' not in session or session['usuario_logado'] == None:
         return redirect(url_for('login', proxima=url_for('perfil')))
     return render_template('cliente_perfil_usuario.html', titulo='Usuarios')
+
 
 @app.route('/mensagens')
 def mensagens():
@@ -112,4 +132,4 @@ def mensagens():
         return redirect(url_for('login', proxima=url_for('perfil')))
     return render_template('cliente_perfil_mensagem.html', titulo='Mensagens')
 
-app.run(debug=True)
+
