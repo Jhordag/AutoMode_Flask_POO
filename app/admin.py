2feb8e9b60c8baf6c2e53e5a958e68e7e237c40f
Perkhad <gabrieldamatamarques@gmail.com>
2021-06-06 21:39:34 -0300

    versaozinha 1.0

--- app/admin.py
+++ app/admin.py
@@ -1,7 +1,14 @@
-from flask import Flask, render_template, request, redirect, session, flash, url_for
+from flask_mysqldb import MySQL
+import os
+import mysql.connector
 from app import app
+from flask import render_template, request, redirect, session, flash, url_for
 
-# app = Flask(__name__)
+db = mysql.connector.connect(host=("lg3bot.cxp5nvrsoub5.us-east-2.rds.amazonaws.com"),
+                             user=("admin"),
+                             password=("Lg3botisaproduct"),
+                             db=("POO"))
+
 app.secret_key = 'BiaUFGPOO'
 
 
@@ -19,7 +26,7 @@ class Cliente:
         self.plano = plano
 
 
-class Funcionario():
+class Funcionario:
     def __init__(self, nome, cpf, salario, cargo):
         self.nome = nome
         self.cpf = cpf
@@ -34,50 +41,11 @@ class Plano:
         self.descricao = descricao
 
 
-# Admins
-admin1 = Admin('luan', 'Luan Marques', '1234')
-admin2 = Admin('Nico', 'Nico Steppat', '7a1')
-admin3 = Admin('flavio', 'flavio Almeida', 'javascript')
-usuarios = {admin1.nome: admin1,
-            admin2.nome: admin2,
-            admin3.nome: admin3}
-
-# Clientes
-cliente1 = Cliente('Loja 67', '46964253000186', 'Premium')
-cliente2 = Cliente('JUSTGOI', '46964253000186', 'Básico')
-cliente3 = Cliente('GOIRF', '46964253000186', 'Intermediario')
-listaclientes = [cliente1, cliente2, cliente3]
-
-# Funcionarios
-funcionario1 = Funcionario('Ana', '1234567890', 3500.00, 'Dev')
-funcionario2 = Funcionario('Pedro', '4234447890', 3500.00, 'Dev')
-funcionario3 = Funcionario('Maria', '4235677890', 3000.00, 'UX')
-trabalhadores = [funcionario1, funcionario2, funcionario3]
-
-# Planos
-plano1 = Plano('Básico', 89.00,
-               'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ornare, turpis vitae faucibus '
-               'tincidunt, erat sem commodo sem, eget dapibus leo nisi non est.')
-plano2 = Plano('Intermediario', 119.00,
-               'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ornare, turpis vitae faucibus '
-               'tincidunt, erat sem commodo sem, eget dapibus leo nisi non est.')
-plano3 = Plano('Premium', 159.00,
-               'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ornare, turpis vitae faucibus '
-               'tincidunt, erat sem commodo sem, eget dapibus leo nisi non est.')
-listaplanos = [plano1, plano2, plano3]
-
-
-@app.route("/")
-@app.route("/index")
-def home():
-    return render_template('index.html')
-
-
 # Home
 @app.route('/admin')
 def index():
-    if 'usuario_logado' not in session or session['usuario_logado'] is None:
-        return redirect(url_for('login', proxima=url_for('index')))
+    if 'usuario_logado' not in session or session['usuario_logado'] == None:
+        return redirect(url_for('admin_login', proxima=url_for('index')))
     return render_template('admin_home.html', titulo='Home Admin')
 
 
@@ -88,18 +56,33 @@ def admin_login():
     return render_template('admin_login.html', proxima=proxima)
 
 
+######################### Gabriel Urzeda #################################
+'''
+Deve usar os dados que estão presentes na tabela Admin para validar a entrada 
+na pagina.
+Lembra-se que tem os funcionarios é os admins então deve ter um redimensionamento 
+de paginas diferentes para eles.
+o def autenticar_cliente() no arquino cliente.py pode ser usado com exemplo
+'''
+
+
+########################################################################
 @app.route('/admin_autenticar', methods=['POST', ])
 def autenticar_admin():
-    if request.form['usuario'] in usuarios:
-        usuario = usuarios[request.form['usuario']]
-        if usuario.senha == request.form['senha']:
-            session['usuario_logado'] = usuario.nome
-            flash(usuario.nome + ' logou com sucesso!')
-            proxima_pagina = request.form['proxima']
-            return redirect(proxima_pagina)
+    cursor = db.cursor()
+    sql1 = f"SELECT * FROM  acessAdmin WHERE Email='{request.form['usuario']}' AND senha='{request.form['senha']}' "
+    cursor.execute(sql1)
+    profile = cursor.fetchall()
+    print(profile)
+
+    if len(profile) != 0:
+        session['usuario_logado'] = profile[0][1]
+        flash(profile[0][1] + ' logou com sucesso!')
+        proxima_pagina = request.form['proxima']
+        return redirect(proxima_pagina)
     else:
         flash('Não logado, tente novamente!')
-        return redirect(url_for('login'))
+        return redirect(url_for('admin_login'))
 
 
 @app.route('/admin_logout')
@@ -110,52 +93,125 @@ def admin_logout():
 
 
 # Rotas Funcionarios
+######################### Gabriel Urzeda #################################
+'''
+Deve Usar as informação da  tabela admin para serem rederizadas 
+para isso deve se usar passar uma lista de todos os usuarios para trabalhadores
+'''
+
+
+########################################################################
 @app.route('/admin_funcionarios')
 def funcionarios():
-    if 'usuario_logado' not in session or session['usuario_logado'] is None:
+    # Funcionarios
+    cursor = db.cursor()
+    sql1 = f"SELECT * FROM  Funcionario"
+    cursor.execute(sql1)
+    trabalhadores = cursor.fetchall()
+    listatrabalhadores = []
+    for i in trabalhadores:
+        listatrabalhadores.append(Funcionario(i[1], i[2], i[3], i[4]))
+
+    if 'usuario_logado' not in session or session['usuario_logado'] == None:
         return redirect(url_for('login', proxima=url_for('admin_funcionarios')))
-    return render_template('admin_funcionario.html', titulo='Funcionarios', trabalhadores=trabalhadores)
+    return render_template('admin_funcionario.html', titulo='Funcionarios', trabalhadores=listatrabalhadores)
 
 
+######################### Gabriel Urzeda #################################
+'''
+Deve Usar a tabela admin para salvar as informações
+A função def salvar_cliente() do arquivo cliente.py pode servir de exemplo
+'''
+
+
+########################################################################
 @app.route('/criarfunc', methods=['POST', ])
 def criarfunc():
-    nome = request.form['nome']
-    cpf = request.form['cpf']
-    salario = request.form['salario']
-    cargo = request.form['cargo']
-    func = Funcionario(nome, cpf, salario, cargo)
-    trabalhadores.append(func)
+    cursor = db.cursor()
+    sql1 = "INSERT INTO Funcionario (nome, cpf, salario, cargo) VALUES(%s, %s, %s, %s)"
+    datas = (request.form['nome'],request.form['cpf'],request.form['salario'],request.form['cargo'])
+    cursor.execute(sql1, datas)
+    db.commit()
     return redirect(url_for('funcionarios'))
 
 
 # Rotas dos Planos
+######################### Gabriel Urzeda #################################
+'''
+Deve Usar as informação da  tabela planos para serem rederizadas 
+para isso deve se usar passar uma lista de todos os planos para listaplanos
+'''
+########################################################################
+
+######################### Gabriel Marques ##############################
+'''
+Deve ser possivel editar ou excluir plano
+'''
+
+
+########################################################################
 @app.route('/admin_planos')
 def planos():
-    if 'usuario_logado' not in session or session['usuario_logado'] is None:
+    # Planos
+    cursor = db.cursor()
+    sql1 = f"SELECT * FROM  Plano"
+    cursor.execute(sql1)
+    planos = cursor.fetchall()
+    listaplanos = []
+    for i in planos:
+        listaplanos.append(Plano(i[1], i[2], i[3]))
+
+    if 'usuario_logado' not in session or session['usuario_logado'] == None:
         return redirect(url_for('login', proxima=url_for('admin_planos')))
     return render_template('admin_planos.html', titulo='Funcionarios', listaplanos=listaplanos)
 
 
 @app.route('/admin_novo_plano')
 def admin_planos():
-    if 'usuario_logado' not in session or session['usuario_logado'] is None:
+    if 'usuario_logado' not in session or session['usuario_logado'] == None:
         return redirect(url_for('login', proxima=url_for('admin_novo_plano')))
     return render_template('admin_criar_plano.html', titulo='Novo Plano')
 
 
+######################### Gabriel Urzeda #################################
+'''
+Deve Usar a tabela plano para salvar as informações
+A função def salvar_cliente() do arquivo cliente.py pode servir de exemplo
+'''
+
+
+########################################################################
 @app.route('/criarplano', methods=['POST', ])
 def criarplano():
-    nome = request.form['nome']
-    preco = request.form['preco']
-    descricao = request.form['descricao']
-    plano = Plano(nome, preco, descricao)
-    listaplanos.append(plano)
+    cursor = db.cursor()
+    sql1 = "INSERT INTO Plano (Nome, Preco, Descricao) VALUES(%s, %s, %s)"
+    datas = (request.form['nome'],request.form['preco'],request.form['descricao'])
+    cursor.execute(sql1, datas)
+    db.commit()
     return redirect(url_for('planos'))
 
 
 # Rotas Clientes
+######################### Gabriel Urzeda #################################
+'''
+Deve Usar as informação da  tabela CLIENTES para serem rederizadas 
+para isso deve se usar passar uma lista de todos os clientes cadastrados 
+para listaclientes
+'''
+
+
+########################################################################
 @app.route('/admin_cliente')
 def cliente():
-    if 'usuario_logado' not in session or session['usuario_logado'] is None:
+    # Clientes
+    cursor = db.cursor()
+    sql1 = f"SELECT * FROM  CLIENTES"
+    cursor.execute(sql1)
+    clientes = cursor.fetchall()
+    listaclientes = []
+    for i in clientes:
+        listaclientes.append(Cliente(i[1], i[2], 'Premium'))
+
+    if 'usuario_logado' not in session or session['usuario_logado'] == None:
         return redirect(url_for('login', proxima=url_for('admin_cliente')))
     return render_template('admin_cliente.html', titulo='Clientes', listaclientes=listaclientes)
--- app/cliente.py
+++ app/cliente.py
@@ -1,28 +1,20 @@
-from flask import Flask, render_template, request, redirect, session, flash, url_for
-from app import app
+from app  import app
+from flask import  render_template, request, redirect, session, flash, url_for
 import mysql.connector
 
+db = mysql.connector.connect(user='admin', password='Lg3botisaproduct',
+                             host='lg3bot.cxp5nvrsoub5.us-east-2.rds.amazonaws.com',
+                             database='POO')
+
 app.secret_key = 'BiaUFGPOO'
-
-
 class Plano:
     def __init__(self, nome, preco, descricao):
         self.nome = nome
         self.preco = preco
         self.descricao = descricao
 
-
-class Cliente:
-    def __init__(self, empresa, cnpj, phone, email, senha):
-        self.empresa = empresa
-        self.cnpj = cnpj
-        self.phone = phone
-        self.email = email
-        self.senha = senha
-
-
 class Cartao:
-    def __init__(self, parcela, numcart, dtv, cvv, nome, cpf):
+    def __init__(self,parcela,numcart,dtv,cvv,nome,cpf):
         self.parcela = parcela
         self.numcart = numcart
         self.dtv = dtv
@@ -30,92 +22,130 @@ class Cartao:
         self.nome = nome
         self.cpf = cpf
 
+class Usuarios:
+    def __init__(self,nome,phone):
+        self.nome = nome
+        self.phone = phone
 
-# Clientes
-cliente1 = Cliente('JOJH', '12345678901234', '99999000011', 'jojh@jojh.com', '1234')
-cliente2 = Cliente('POJ', '12345278921234', '99999000022', 'poj@poj.com', '1357')
-cliente3 = Cliente('KOL', '12345178411234', '99999000033', 'poj@poj.com', '2468')
-clientes = {cliente1.email: cliente1,
-            cliente2.email: cliente2,
-            cliente3.email: cliente3}
-
-# Planos
-plano1 = Plano('Básico', 89.00,
-               'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ornare, turpis vitae faucibus '
-               'tincidunt, erat sem commodo sem, eget dapibus leo nisi non est.')
-plano2 = Plano('Intermediario', 119.00,
-               'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ornare, turpis vitae faucibus '
-               'tincidunt, erat sem commodo sem, eget dapibus leo nisi non est.')
-plano3 = Plano('Premium', 159.00,
-               'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ornare, turpis vitae faucibus '
-               'tincidunt, erat sem commodo sem, eget dapibus leo nisi non est.')
-listaplanos = [plano1, plano2, plano3]
-
+class Cadastro:
+    def __init__(self,nome_empresa,cnpj,phone,email,senha):
+        self.nome_empresa = nome_empresa
+        self.cnpj = cnpj
+        self.phone = phone
+        self.email = email
+        self.senha = senha
 
 @app.route("/")
-@app.route("/index")
 def inicio():
     return render_template('index.html')
 
-
 # Home Cliente
-@app.route('/cliente')
-def home_cliente():
+######################### Gabriel Urzeda #################################
+'''
+Deve Usar as informação da  tabela planos para serem rederizadas 
+para isso deve se usar passar uma lista de todos os planos para listaplanos
+'''
+########################################################################
+@app.route('/home')
+def home():
+    # Planos
+    cursor = db.cursor()
+    sql1 = f"SELECT * FROM  Plano"
+    cursor.execute(sql1)
+    planos = cursor.fetchall()
+    listaplanos = []
+    for i in planos:
+        listaplanos.append(Plano(i[1], i[2], i[3]))
+
     return render_template('cliente_home.html', titulo='Planos', listaplanos=listaplanos)
-
-
+######################### Gabriel Urzeda #################################
+'''
+Deve Usar as informação da  tabela planos para serem rederizadas 
+para isso deve se usar passar uma lista de todos os planos para listaplanos
+'''
+########################################################################
 @app.route('/home_login')
 def home_login():
+    # Planos
+    cursor = db.cursor()
+    sql1 = f"SELECT * FROM  Plano"
+    cursor.execute(sql1)
+    planos = cursor.fetchall()
+    listaplanos = []
+    for i in planos:
+        listaplanos.append(Plano(i[1], i[2], i[3]))
+
     if 'usuario_logado' not in session or session['usuario_logado'] == None:
         return redirect(url_for('login', proxima=url_for('home_login')))
     return render_template('cliente_home_login.html', titulo='Home', listaplanos=listaplanos)
 
-
-# Autenticação
+#Autenticação
 @app.route('/login')
 def login():
     proxima = request.args.get('proxima')
-    return render_template('cliente_login.html', proxima=proxima)
-
+    return render_template('cliente_login.html',proxima=proxima)
 
 @app.route('/cliente_autenticar', methods=['POST', ])
 def autenticar_cliente():
-    if request.form['usuario'] in clientes:
-        usuario = clientes[request.form['usuario']]
-        if usuario.senha == request.form['senha']:
-            session['usuario_logado'] = usuario.email
-            flash(usuario.email + ' logou com sucesso!')
-            proxima_pagina = request.form['proxima']
-            return redirect(proxima_pagina)
+    cursor = db.cursor()
+    sql1 = f"SELECT * FROM  CLIENTES WHERE Email='{request.form['usuario']}' AND Senha='{request.form['senha']}' "
+    cursor.execute(sql1)
+    profile = cursor.fetchall()
+    print(profile)
+
+    if len(profile) != 0:
+        print('Entrou')
+        session['usuario_logado'] = profile[0][1]
+        flash(profile[0][1] + ' logou com sucesso!')
+        proxima_pagina = request.form['proxima']
+        return redirect(proxima_pagina)
     else:
         flash('Não logado, tente novamente!')
         return redirect(url_for('login'))
 
-
+    
 @app.route('/logout')
 def logout():
     session['usuario_logado'] = None
     flash('Nenhum usuário logado!')
     return redirect(url_for('home'))
 
+#Cadastro 
+@app.route('/cadastro')
+def cadastro():
+    return render_template('cliente_cadastro.html', titulo='Cadastro Cliente')
 
-# Comprar Plano
+@app.route('/salvarcadastro',methods=['POST', ])
+def salvar_cliente():
+    cursor = db.cursor()
+    sql1 = "INSERT INTO CLIENTES (Nome_Empresa, CNPJ, Phone, Email, Senha) VALUES(%s, %s, %s,%s,%s)"
+    datas = (request.form['nome_emp'],request.form['cnpj'],request.form['phone'],request.form['email'],request.form['senha'])
+    cursor.execute(sql1, datas)
+    db.commit()
+    return redirect(url_for('home'))
+
+
+#Comprar Plano
 @app.route('/comprarcredit')
 def comprar():
-    if 'usuario_logado' not in session or session['usuario_logado'] is None:
+    if 'usuario_logado' not in session or session['usuario_logado'] == None:
         return redirect(url_for('login', proxima=url_for('home_login')))
     return render_template('cliente_comprar.html', titulo='Comprar')
 
-
+######################### Gabriel Marques ##############################
+'''
+Deve fazer aparecer uma mensagen de compra feita com sucesso
+exemplo def autenticar_cliente() e o cliente_login.html
+'''
+########################################################################
 @app.route('/autenticar_cartao', methods=['POST', ])
 def autenticar_cartao():
-    if request.form['numcart'] is not None:
+    if request.form['numcart'] != None :
         flash('Compara feita  com sucesso!')
         return redirect(url_for('home_login'))
     else:
         flash('Não foi possivel fazer a comprar!')
         return redirect(url_for('login'))
-
 
 # Perfil Cliente
 @app.route('/perfil')
@@ -124,39 +154,71 @@ def perfil():
         return redirect(url_for('login', proxima=url_for('perfil')))
     return render_template('cliente_perfil.html', titulo='Perfil')
 
+######################### Gabriel Urzeda #################################
+'''
+Deve Usar as informação da  tabela usuarios para serem rederizadas 
+para isso deve se usar passar uma lista de todos os usuarios para users_cliente
+'''
+########################################################################
 
+######################### Gabriel Marques ##############################
+'''
+Deve fazer aparecer uma mensagen de usuario cadastrado com sucesso
+exemplo def autenticar_cliente() e o cliente_login.html
+'''
+########################################################################
 @app.route('/usuarios')
 def usuarios():
+    # Usuarios
+    cursor = db.cursor()
+    sql1 = f"SELECT * FROM  CLIENTES"
+    cursor.execute(sql1)
+    users = cursor.fetchall()
+    users_cliente = []
+    for i in users:
+        users_cliente.append(Usuarios(i[1], i[2]))
+
     if 'usuario_logado' not in session or session['usuario_logado'] == None:
         return redirect(url_for('login', proxima=url_for('perfil')))
-    return render_template('cliente_perfil_usuario.html', titulo='Usuarios')
+    return render_template('cliente_perfil_usuario.html', titulo='Usuarios',users_cliente = users_cliente)
 
+######################### Gabriel Urzeda #################################
+'''
+Deve Usar a tabela usuarios para salvar as informações
+A função def salvar_cliente() do arquivo cliente.py pode servir de exemplo
+'''
+########################################################################
+@app.route('/cadastrousuario', methods=['POST',])
+def criarusuarios():
+    nome = request. form['nome']
+    phone= request. form['phone']
+    cursor = db.cursor()
+    sql1 = "INSERT INTO Usuarios (Nome, Phone) VALUES('%s', '%s')"
+    datas = (request.form['Nome'],request.form['Phone'])
+    cursor.execute(sql1, datas)
+    db.commit()
+    return redirect(url_for('usuarios'))
 
+######################### Gabriel Urzeda #################################
+'''
+Deve Usar a tabela mensagem para salvar as informações
+A função def salvar_cliente() do arquivo cliente.py pode servir de exemplo
+'''
+########################################################################
+
+######################### Gabriel Marques ##############################
+'''
+Deve fazer aparecer uma mensagen de mensagens enviadas com sucesso
+exemplo def autenticar_cliente() e o cliente_login.html
+'''
+########################################################################
 @app.route('/mensagens')
 def mensagens():
     if 'usuario_logado' not in session or session['usuario_logado'] == None:
         return redirect(url_for('login', proxima=url_for('perfil')))
     return render_template('cliente_perfil_mensagem.html', titulo='Mensagens')
 
-
-# Cadastro
-@app.route('/cadastro')
-def cadastro():
-    return render_template('cliente_cadastro.html', titulo='Cadastro Cliente')
-
-
-@app.route('/salvarcadastro', methods=['POST', ])
-def salvar_cliente():
-    db = mysql.connector.connect(user='admin', password='Lg3botisaproduct',
-                                 host='lg3bot.cxp5nvrsoub5.us-east-2.rds.amazonaws.com',
-                                 database='POO')
-
-    cursor = db.cursor()
-    sql1 = "INSERT INTO CLIENTES (Nome_Empresa, CNPJ, Phone, Email, Senha) VALUES(%s, %s, %s,%s,%s)"
-    datas = (
-        request.form['nome_emp'], request.form['cnpj'], request.form['phone'], request.form['email'],
-        request.form['senha'])
-    cursor.execute(sql1, datas)
-    db.commit()
-    cursor.close()
-    return redirect(url_for('home'))
+@app.route('/validarmensagem', methods=['POST',])
+def validarmensagem():
+    flash('Mensagem enviada com sucesso!')
+    return redirect(url_for('mensagens'))
